e4dc49b201679f845105b4dbe2da857ae67dd235
Ali <amchoclaty@gmail.com>
2025-04-10 19:00:23 +0500

    add class  test3

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,5 +1,5 @@
 import "@/assets/css/test.css";
 const Test = () => {
-  return <div className="test test2">Project Run Successfully...</div>;
+  return <div className="test test2 test3 ">Project Run Successfully...</div>;
 };
 export default Test;
